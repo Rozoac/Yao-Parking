9620fffe97d06068cac42551b6c5762283a6abb2
Juan Rozo <rozoacg@gmail.com>
2017-03-17 17:41:11 -0500

    Login - Admin

---
+++ resources/views/main/home.blade.php
@@ -0,0 +1,34 @@
+@extends('layouts.app')
+
+@section('title', 'Pagina Principal')
+
+@section('content')
+
+<body id="page-top" class="index">
+
+    <header style=" background: url(http://www.trazosyconcreto.com/wp-content/uploads/2015/11/0032.jpg);">
+        <div class="container"  >
+            <div class="intro-text">
+                <div class="intro-lead-in">Procesos Administrativos de la Universidad El Bosque
+                <div class="intro-heading">Simulador de Parqueaderos</div>
+                <a  class="page-scroll btn btn-xl" style="background: #c68f00">Más Información</a>
+            </div>
+        </div>
+    </header>
+
+    
+
+    <footer style="background: black">
+        <div class="container ">
+            <div class="row">
+                <div class="col-md-4">
+                    <span class="copyright" style="color: white;">Copyright &copy; Yao-parking 2017</span>
+                </div>
+                
+                
+            </div>
+        </div>
+    </footer>
+
+
+@endsection
